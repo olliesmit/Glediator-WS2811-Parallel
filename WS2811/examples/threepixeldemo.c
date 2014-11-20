1f8c035801efddb0e1423db388e1bd5ad0b1e19d
olliesmit <osmizzle@gmail.com>
2014-11-20 02:39:01 +0000

    Revert "Revert "NOT MINE""
    This reverts commit ef4c9aa414fdd74cd7050dd3f8ac74943f301ddc.

---
+++ WS2811/examples/threepixeldemo.c
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2012 Alan Burlison, alan@bleaklow.com.  All rights reserved.
+ * Use is subject to license terms.
+ *
+ * Demo of the WS2811 driver, driving 3 pixels.
+ * Compile in GNU C99 mode (-std=gnu99)
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <WS2811.h>
+
+// Adjust as necessary.
+#define PORT  PORTB
+#define DDR    DDRB
+#define PIN       0
+#define PAUSE  1000     // msec
+#define DELAY    10	// msec
+
+// Bit twiddling macros.
+#define BIT(B)           (0x01 << (uint8_t)(B))
+#define SET_BIT_HI(V, B) (V) |= (uint8_t)BIT(B)
+#define SET_BIT_LO(V, B) (V) &= (uint8_t)~BIT(B)
+ 
+// Define the output function.
+DEFINE_WS2811_OUT_1_FN(WS2811RGB, PORT, PIN)
+
+// Drive the three pixels in an infinite loop.
+void threepixeldemo(void)
+{
+    // Configure pin for output.
+    SET_BIT_HI(DDR, PIN);
+    SET_BIT_LO(PORT, PIN);
+
+    // off->red, off->green, off->blue
+    RGB_t rgb[3] = {{0,0,0},{0,0,0},{0,0,0}};
+    WS2811RGB(rgb, ARRAYLEN(rgb));
+    _delay_ms(PAUSE);
+    for (int i = 0; i < 255; i++) {
+        rgb[0].r += 1; 
+        rgb[1].g += 1; 
+        rgb[2].b += 1; 
+        WS2811RGB(rgb, ARRAYLEN(rgb));
+        _delay_ms(DELAY);
+    }
+
+    // loop forever.
+    for (;;) {
+        // red->yellow, green->cyan, blue->magenta    
+        for (int i = 0; i < 255; i++) {
+            rgb[0].g += 1; 
+            rgb[1].b += 1; 
+            rgb[2].r += 1; 
+            WS2811RGB(rgb, ARRAYLEN(rgb));
+            _delay_ms(DELAY);
+        }
+        // yellow->green, cyan->blue, magenta->white
+        for (int i = 0; i < 255; i++) {
+            rgb[0].r -= 1; 
+            rgb[1].g -= 1; 
+            rgb[2].g += 1; 
+            WS2811RGB(rgb, ARRAYLEN(rgb));
+            _delay_ms(DELAY);
+        }
+        // green->cyan, blue->magenta, white->red
+        for (int i = 0; i < 255; i++) {
+            rgb[0].b += 1; 
+            rgb[1].r += 1; 
+            rgb[2].g -= 1; 
+            rgb[2].b -= 1; 
+            WS2811RGB(rgb, ARRAYLEN(rgb));
+            _delay_ms(DELAY);
+        }
+        // cyan->blue, magenta->white, red->yellow
+        for (int i = 0; i < 255; i++) {
+            rgb[0].g -= 1;
+            rgb[1].g += 1;
+            rgb[2].g += 1;
+            WS2811RGB(rgb, ARRAYLEN(rgb));
+            _delay_ms(DELAY);
+        }
+        // blue->magenta, white->red, yellow->green
+        for (int i = 0; i < 255; i++) {
+            rgb[0].r += 1;
+            rgb[1].g -= 1;
+            rgb[1].b -= 1;
+            rgb[2].r -= 1;
+            WS2811RGB(rgb, ARRAYLEN(rgb));
+            _delay_ms(DELAY);
+        }
+        // magenta->white, red->yellow, green->cyan
+        for (int i = 0; i < 255; i++) {
+            rgb[0].g += 1;
+            rgb[1].g += 1;
+            rgb[2].b += 1;
+            WS2811RGB(rgb, ARRAYLEN(rgb));
+            _delay_ms(DELAY);
+        }
+        // white->red, yellow->green, cyan->blue
+        for (int i = 0; i < 255; i++) {
+            rgb[0].b -= 1;
+            rgb[0].g -= 1;
+            rgb[1].r -= 1;
+            rgb[2].g -= 1;
+            WS2811RGB(rgb, ARRAYLEN(rgb));
+            _delay_ms(DELAY);
+        }
+    }
+}
